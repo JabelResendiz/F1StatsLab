dc759d1dfd2982e7f784a7e11391428ef1a5b700
JabelResendiz <jabelresendiz18@gmail.com>
2025-01-23 20:29:42 -0500

    some changes

--- estudy.py
+++ estudy.py
@@ -8,16 +8,17 @@ from scipy import stats
 # Cargar los datos
 df = pd.read_csv('./formula1/formula1_race_data.csv')
 
-print(df["Driver"].value_counts())
 
+driver= df[df["Driver"]=='Hamilton']
 
-df.head(10).plot(kind='bar', figsize=(10, 6), color='skyblue')
-plt.title('Top 10 Pilotos con Más Participaciones')
-plt.ylabel('Número de Participaciones')
-plt.xlabel('Piloto')
-plt.show()
+# Asegurarse de que RaceDate sea de tipo fecha
+df["Date"] = pd.to_datetime(df["Date"])
 
+# Crear una columna de año
+df["Year"] = df["Date"].dt.year
 
+# Agrupar por año y sumar los adelantamientos
+overtakes_per_year = df.groupby("Year")["Overtakes"].sum()
 
-dnf_rate = df.groupby("Driver")["DNF"].mean()
-print(dnf_rate.sort_values(ascending=False).head(10))
+# Mostrar los adelantamientos por año
+print(overtakes_per_year)
